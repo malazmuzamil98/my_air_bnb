edce8a2929c2229e9f318a8743971f662a7ce8f3
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-05-20 00:01:50 +0300

    All

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Amenity class"""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """Amenity class"""
-
-    name = ""
--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""a class that is specialized in states"""
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """State class"""
-
-    name = ""
